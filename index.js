e4bc996160974461a8abbb2f1c6444585335b111
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-08-13 14:10:14 -0400

    array lab completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push("Ralph");
+};
+
+function destructivelyPrependCat(name){
+    return cats.unshift("Bob");
+};
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+};
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+};
+
+function appendCat(name){
+    const Broom = [...cats,"Broom"];
+    return Broom;
+};
+
+function prependCat(name){
+    const Arnold = ["Arnold", ...cats];
+    return Arnold;
+};
+
+function removeLastCat(){
+    return cats.slice(0,-1);
+};
+
+function removeFirstCat(){
+    return cats.slice(1);
+};
+
